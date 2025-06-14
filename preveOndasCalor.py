10f7f1d2a2831a79ed1ff3074bfb48b85fec36ff
brjatoba92 <brjatoba92@gmail.com>
2025-06-14 18:51:56 -0300

    Dependencias importadas e setar configurações iniciais

---
+++ preveOndasCalor.py
@@ -0,0 +1,13 @@
+# Importação das dependencias necessarias
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+import calendar
+from datetime import datetime, timedelta
+
+# Configurações Iniciais
+plt.style.use('ggplot')
+sns.set_palette("YlOrRd")
